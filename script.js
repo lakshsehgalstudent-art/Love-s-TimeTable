9f8abced05fdf14bac0dc76f870201af19d033be
lakshsehgalstudent-art <laksh.sehgal.student@srcc.du.ac.in>
2026-04-09 23:46:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 const data = {
+
   mon: [
-    {time:"8:45 AM", subject:"HRM", room:"202", teacher:"SC"},
-    {time:"2:05 PM", subject:"GE II", room:"T3-21", teacher:"DT"}
+    {time:"2:05 PM", subject:"GE II", room:"T3-21", teacher:"DT"},
+    {time:"3:05 PM", subject:"EVS", room:"216", teacher:"-"}
   ],
-  tue: [],
-  wed: [],
-  thu: [],
-  fri: []
+
+  tue: [
+    {time:"9:45 AM", subject:"HRM Tutorial", room:"G1", teacher:"SC"},
+    {time:"10:45 AM", subject:"HRM", room:"G2", teacher:"SB"},
+    {time:"2:05 PM", subject:"DSC 2.1", room:"202", teacher:"DM"},
+    {time:"3:05 PM", subject:"DSC 2.2", room:"202", teacher:"SB"}
+  ],
+
+  wed: [
+    {time:"8:45 AM", subject:"GE II", room:"T3-21", teacher:"DT"},
+    {time:"9:45 AM", subject:"DSC 2.3", room:"202", teacher:"SB"},
+    {time:"10:45 AM", subject:"DSC 2.2", room:"219", teacher:"SB"},
+    {time:"11:45 AM", subject:"HRM Tutorial", room:"205", teacher:"SB"},
+    {time:"2:05 PM", subject:"HRM Tutorial", room:"X7", teacher:"SC"}
+  ],
+
+  thu: [
+    {time:"8:45 AM", subject:"AEC", room:"202", teacher:"EVS"},
+    {time:"9:45 AM", subject:"DSC 2.3", room:"202", teacher:"SC"},
+    {time:"10:45 AM", subject:"Tutorial", room:"CR-307", teacher:"-"},
+    {time:"11:45 AM", subject:"Tutorial", room:"207", teacher:"-"}
+  ],
+
+  fri: [
+    {time:"8:45 AM", subject:"DSC 2.1", room:"201", teacher:"DM"},
+    {time:"9:45 AM", subject:"Break", room:"-", teacher:"-"},
+    {time:"10:45 AM", subject:"Tutorial", room:"SB-205", teacher:"-"},
+    {time:"11:45 AM", subject:"DSC 2.2", room:"321", teacher:"SB"},
+    {time:"2:05 PM", subject:"GE II", room:"T3-01", teacher:"DT"},
+    {time:"3:05 PM", subject:"GE (ANG)", room:"-", teacher:"ANG"}
+  ],
+
+  sat: [
+    {time:"Morning", subject:"SEC", room:"-", teacher:"-"},
+    {time:"Midday", subject:"VAC", room:"-", teacher:"-"},
+    {time:"Afternoon", subject:"AEC", room:"-", teacher:"-"}
+  ]
+
 };
-
-function showDay(day) {
-  const container = document.getElementById("schedule");
-  container.innerHTML = "";
-
-  if (!data[day] || data[day].length === 0) {
-    container.innerHTML = "<p>No classes</p>";
-    return;
-  }
-
-  data[day].forEach(item => {
-    container.innerHTML += `
-      <div class="card">
-        <h3>${item.subject}</h3>
-        <p>⏰ ${item.time}</p>
-        <p>📍 Room ${item.room}</p>
-        <p>👤 ${item.teacher}</p>
-      </div>
-    `;
-  });
-}
-
-// default load
-showDay('mon');
